800c540dce72faefe9e506c8de20fd9c13af55a4
Jannes <jannes.meyer@gmail.com>
2015-04-19 14:43:46 +0200

    Throw when executing inside of node

--- Readme.md
+++ Readme.md
@@ -4,14 +4,28 @@
 [![Dependencies](https://david-dm.org/JannesMeyer/xhr-tool.svg)](https://david-dm.org/JannesMeyer/xhr-tool)
 [![Development Dependencies](https://david-dm.org/JannesMeyer/xhr-tool/dev-status.svg)](https://david-dm.org/JannesMeyer/xhr-tool#info=devDependencies)
 
+Install the package like this:
+
+	npm install xhr-tool
+
 **Import the function**
 
 ~~~js
 var getJSON = require('xhr-tool').getJSON;
 ~~~
 
+Needs a browser that supports Promises or a Promise polyfill.
+
 ## getJSON
 
 	getJSON(url) → Promise → Object
 
 Sends an asynchronous request and returns a Promise for the parsed JSON response
+
+Example usage:
+
+~~~js
+getJSON('http://www.nactem.ac.uk/software/acromine/dictionary.py?sf=BMI').then(function(obj) {
+	console.log(obj);
+});
+~~~
--- xhr-tool.js
+++ xhr-tool.js
@@ -3,23 +3,27 @@
  */
 function getJSON(url) {
 	var res = Promise.defer();
-	var req = new XMLHttpRequest();
-	req.onload = function() {
-		try {
-			res.resolve(JSON.parse(req.response));
-		} catch (err) {
-			res.reject(err);
-		}
-	};
-	req.onerror = function() {
-		res.reject(new Error('Error ' + req.status));
-	};
-	req.ontimeout = function() {
-		res.reject(new Error('Request timeout'));
-	};
-	req.open('GET', url);
-	req.setRequestHeader('Accept', 'application/json');
-	req.send();
+	if (typeof XMLHttpRequest !== 'undefined') {
+		var req = new XMLHttpRequest();
+		req.onload = function() {
+			try {
+				res.resolve(JSON.parse(req.response));
+			} catch (err) {
+				res.reject(err);
+			}
+		};
+		req.onerror = function() {
+			res.reject(new Error('Error ' + req.status));
+		};
+		req.ontimeout = function() {
+			res.reject(new Error('Request timeout'));
+		};
+		req.open('GET', url);
+		req.setRequestHeader('Accept', 'application/json');
+		req.send();
+	} else {
+		res.reject(new Error('XMLHttpRequest not available'));
+	}
 	return res;
 }
 exports.getJSON = getJSON;
