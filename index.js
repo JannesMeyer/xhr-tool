5fd7c9d960078b0cb3886acccbc2e099b57a9601
Jannes <jannes.meyer@gmail.com>
2015-05-21 22:04:26 +0200

    Stop using Promise.defer() because it's not supported on Firefox

---
+++ index.js
@@ -0,0 +1,29 @@
+/**
+ * Sends an asynchronous request and returns a Promise for the parsed JSON response
+ *
+ * @param url The URL to fetch and parse
+ */
+function getJSON(url) {
+	return new Promise(function(resolve, reject) {
+		if (typeof url !== 'string') {
+			reject(new TypeError('First argument has to be a string'));
+			return;
+		}
+		if (typeof XMLHttpRequest === 'undefined') {
+			reject(new Error('XMLHttpRequest not available'));
+			return;
+		}
+		// Build XmlHttpRequest object
+		var req = new XMLHttpRequest();
+		req.onload = function() {
+			resolve(JSON.parse(req.response));
+		};
+		req.onerror = reject.bind(null, new Error('Error ' + req.status));
+		req.ontimeout = reject.bind(null, new Error('Request timeout'));
+		req.open('GET', url);
+		req.setRequestHeader('Accept', 'application/json');
+		// Send request
+		req.send();
+	});
+}
+exports.getJSON = getJSON;
